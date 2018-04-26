577dbfed710421325718941f66cfda97d02c4871
David Allen <dallen@zendesk.com>
2018-04-26 21:53:50 +1000

    Add button to control run timer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      gameBoard: [{x: 5, y: 5}, {x: 6, y: 5}, {x: 7, y: 5}]
+      gameBoard: [{x: 5, y: 5}, {x: 6, y: 5}, {x: 7, y: 5}, {x: 7, y: 4}, {x: 6, y: 3}],
+      running: false,
+      intervalId: null
     };
   }
 
@@ -15,32 +17,33 @@ class App extends Component {
     this.setState({gameBoard: nextGeneration(this.state.gameBoard)})
   }
 
-  componentDidMount() {
-    sleep(500).then(() => {
-      this.nextBoard();
-    });
-  }
+  handleClick() {
+    let running = this.state.running, intervalId;
 
-  componentDidUpdate() {
-    sleep(500).then(() => {
-      this.nextBoard();
-    });
+    if(running) {
+      clearInterval(this.state.intervalId);
+      this.setState({ intervalId: null, running: !running })
+    } else {
+      intervalId = setInterval(this.nextBoard.bind(this), 300);
+      this.setState({ intervalId, running: !running });
+    }
   }
 
   render() {
     return (
       <div className="game-of-life">
         <Board
-          size="10"
+          size="20"
           board={ this.state.gameBoard }
         />
+        <button onClick={ () => this.handleClick() }>
+          { this.state.running ? 'Stop' : 'Start' }
+        </button>
       </div>
     );
   }
 }
 
-const sleep = ms => (
-  new Promise(resolve => setTimeout(resolve, ms))
-);
+
 
 export default App;
